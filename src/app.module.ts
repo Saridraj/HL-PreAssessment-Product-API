8dffb8dbe46f4e9dc91029155448eb25b31bf3e0
Saridraj <saridrajsapsin@gmail.com>
2025-02-21 00:27:19 +0700

    feat: 🎸 add product operation and connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductModule } from './product/product.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'product-api',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
 
-    ProductModule
+    ProductModule 
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {} 
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ProductController } from './product.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from '../entity/product.entity';
+import { ProductTranslation } from '../entity/productTranslation.entity';
 import { ProductService } from './product.service';
+import { ProductController } from './product.controller';
+import { ProductTranslationService } from './product.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product, ProductTranslation])],
+  providers: [ProductService, ProductTranslationService],
   controllers: [ProductController],
-  providers: [ProductService]
 })
 export class ProductModule {}
